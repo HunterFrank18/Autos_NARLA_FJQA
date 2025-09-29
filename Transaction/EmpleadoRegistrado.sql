cc11c7126f381f14bacfe863ce117f6a833055bc
Mr Castle <134983817+HunterFrank18@users.noreply.github.com>
2025-09-28 18:23:51 -0600

    Add files via upload

---
+++ Transaction/EmpleadoRegistrado.sql
@@ -0,0 +1,26 @@
+﻿use AutosNARLA
+
+--Registrar un empleado con una venta (Empleado + Venta)
+
+BEGIN TRANSACTION;
+
+BEGIN TRY
+    -- 1. Insertar el nuevo empleado
+    INSERT INTO Empleado (EmpleadoID, Nombre, Cargo)  -- Se debe de cambiar el nombre completo del empleado y el ID de este tanto en este campo como en Venta							
+    VALUES (81, 'Carlos Gómez', 'Vendedor');          -- Es decir el valor de EmpleadoID de la tabla Empleado es 81 y para que funcione se debe cambiar
+	                                                  --  El ID del empleado en la insercion para la tabla Venta Venta
+
+    -- 2. Insertar la venta realizada por el empleado
+    INSERT INTO Venta (VentaID, AutoID, ClienteID, EmpleadoID, FechaVenta, PrecioFinal)
+    VALUES (401, 6, 12, 81, GETDATE(), 60104);
+
+    -- Confirmar transacción si todo salió bien
+    COMMIT TRANSACTION;
+    PRINT '✅ Empleado registrado y venta asociada realizada correctamente.';
+END TRY
+BEGIN CATCH
+    -- Revertir si algo falla
+    ROLLBACK TRANSACTION;
+    PRINT '❌ Error: Se deshizo la transacción.';
+    PRINT ERROR_MESSAGE();
+END CATCH;
---
+++ Transaction/RegistrandoClientes.sql
@@ -0,0 +1,25 @@
+﻿use AutosNARLA;
+
+-- . Registrar un cliente nuevo con su primera compra (Cliente + Venta)
+
+BEGIN TRANSACTION;
+
+BEGIN TRY
+    -- 1. Insertar el nuevo cliente
+    INSERT INTO Cliente (ClienteID, Nombre, Apellido,Correo, Telefono)
+    VALUES (101, 'Juan', 'Pérez', 'juan.perez@mail.com', '55512345');
+
+    -- 2. Insertar la venta del cliente
+    INSERT INTO Venta (VentaID, AutoID, ClienteID, EmpleadoID, FechaVenta, PrecioFinal)
+    VALUES (201, 5, 101, 3, GETDATE(), 50204);
+
+    -- Confirmar transacción si todo salió bien
+    COMMIT TRANSACTION;
+    PRINT '✅ Cliente registrado y primera venta realizada correctamente.';
+END TRY
+BEGIN CATCH
+    -- Revertir si algo falla
+    ROLLBACK TRANSACTION;
+    PRINT '❌ Error: Se deshizo la transacción.';
+    PRINT ERROR_MESSAGE();
+END CATCH;
